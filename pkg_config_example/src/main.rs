1d171955969c532b466dc361546852b0c8044eda
imxood <imxood@gmail.com>
2021-10-29 22:50:10 +0800

    .

---
+++ pkg_config_example/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("dir: {:?}", std::env::var("PKG_CONFIG_PATH"));
+    let library = pkg_config::probe_library("realsense2")
+        .expect("pkg-config failed to find realsense2 package");
+    println!("library: {:?}", &library);
+}
