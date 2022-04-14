9c59cb5be02b4751213fcdcbc22a2b48e0c25b4f
imxood <imxood@gmail.com>
2022-04-14 15:29:25 +0800

    .

--- rust_call_c_lib/build.rs
+++ rust_call_c_lib/build.rs
@@ -1,4 +1,13 @@
 fn main() {
     let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}/c_library/build", path);
+ 
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-search=native={}/c_library/build/Debug", path);
+    }
+    
+    #[cfg(target_os = "linux")]
+    {
+        println!("cargo:rustc-link-search=native={}/c_library/build", path);
+    }
 }
--- rust_call_c_lib/c_library/build.sh
+++
@@ -1,4 +0,0 @@
-mkdir build -p
-cd build
-cmake ..
-make
