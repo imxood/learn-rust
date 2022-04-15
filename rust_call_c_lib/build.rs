d2c3f61f028d42b35015504ec2ede4f4647414b7
imxood <imxood@gmail.com>
2022-04-15 15:01:22 +0800

    .

--- rust_call_c_lib/build.rs
+++ rust_call_c_lib/build.rs
@@ -1,9 +1,25 @@
 fn main() {
     let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    
-    // 添加库路径
-    println!("cargo:rustc-link-search=native={}/c_library/out", path);
-    println!("cargo:rustc-link-search=native={}/rust_library/target/debug", path);
+
+    #[cfg(target_os = "windows")]
+    {
+        // 添加 add library 路径
+        let add_library_dir = format!("{}/c_library/out/Debug", &path);
+        println!("cargo:rustc-link-search=native={}", add_library_dir);
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        // 添加 add library 路径
+        let add_library_dir = format!("{}/c_library/out", &path);
+        println!("cargo:rustc-link-search=native={}", add_library_dir);
+    }
+
+    // 添加 minus library 路径
+    println!(
+        "cargo:rustc-link-search=native={}/rust_library/target/debug",
+        &path
+    );
 
     // 添加 静态库或动态库 的名称
     println!("cargo:rustc-link-lib=dylib=add");
--- rust_call_c_lib/c_library/Readme.md
+++ rust_call_c_lib/c_library/Readme.md
@@ -4,3 +4,5 @@
 cmake -B build
 cmake --build build
 ```
+
+输出目录在 out 目录下
