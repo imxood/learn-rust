cc23729b1574de25e9e4d72011a1f9b7450d0af3
imxood <imxood@gmail.com>
2022-04-15 14:35:19 +0800

    .

--- rust_call_c_lib/build.rs
+++ rust_call_c_lib/build.rs
@@ -1,19 +1,11 @@
 fn main() {
     let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
- 
-    #[cfg(target_os = "windows")]
-    {
-        println!("cargo:rustc-link-search=native={}/c_library/build/Debug", path);
-        println!("cargo:rustc-link-search=native={}/rust_library/target/debug", path);
-        println!("cargo:rustc-link-lib=dylib=add");
-        println!("cargo:rustc-link-lib=dylib=minus");
-    }
     
-    #[cfg(target_os = "linux")]
-    {
-        println!("cargo:rustc-link-search=native={}/c_library/build", path);
-        println!("cargo:rustc-link-search=native={}/rust_library/target/debug", path);
-        println!("cargo:rustc-link-lib=dylib=add");
-        println!("cargo:rustc-link-lib=dylib=minus");
-    }
+    // 添加库路径
+    println!("cargo:rustc-link-search=native={}/c_library/out", path);
+    println!("cargo:rustc-link-search=native={}/rust_library/target/debug", path);
+
+    // 添加 静态库或动态库 的名称
+    println!("cargo:rustc-link-lib=dylib=add");
+    println!("cargo:rustc-link-lib=dylib=minus");
 }
