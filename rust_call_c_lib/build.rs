c3e806bcec75b2d77ba1ccd5a0f989575e53cb68
imxood <imxood@gmail.com>
2022-04-15 15:08:51 +0800

    .

--- rust_call_c_lib/build.rs
+++ rust_call_c_lib/build.rs
@@ -1,27 +1,34 @@
 fn main() {
     let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    #[cfg(target_os = "windows")]
-    {
-        // 添加 add library 路径
-        let add_library_dir = format!("{}/c_library/out/Debug", &path);
-        println!("cargo:rustc-link-search=native={}", add_library_dir);
-    }
-
     #[cfg(target_os = "linux")]
     {
         // 添加 add library 路径
-        let add_library_dir = format!("{}/c_library/out", &path);
-        println!("cargo:rustc-link-search=native={}", add_library_dir);
+        println!("cargo:rustc-link-search=native={}/c_library/out", &path);
+        println!(
+            "cargo:rustc-link-search=native={}/rust_library/target/debug",
+            &path
+        );
+
+        // 添加 静态库或动态库 的名称
+        println!("cargo:rustc-link-lib=dylib=minus");
+        println!("cargo:rustc-link-lib=dylib=add");
     }
 
-    // 添加 minus library 路径
-    println!(
-        "cargo:rustc-link-search=native={}/rust_library/target/debug",
-        &path
-    );
+    #[cfg(target_os = "windows")]
+    {
+        // 添加 library 路径
+        println!(
+            "cargo:rustc-link-search=native={}/c_library/out/Debug",
+            &path
+        );
+        println!(
+            "cargo:rustc-link-search=native={}/rust_library/target/debug",
+            &path
+        );
 
-    // 添加 静态库或动态库 的名称
-    println!("cargo:rustc-link-lib=dylib=add");
-    println!("cargo:rustc-link-lib=dylib=minus");
+        // 添加 库名字
+        println!("cargo:rustc-link-lib=dylib=add");
+        println!("cargo:rustc-link-lib=dylib=minus.dll");
+    }
 }
